fe3a918ccc64e06f9c7d08529ee88687bfa6eba0
Danieljusten <daniel.justen@escola.pr.gov.br>
2026-08-18 11:18:51 -0300

    Remove commented SDK import and initialize Firebase

--- firebase-init.js
+++ firebase-init.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDYtJPl1dqwrfhg3ITzWcLJtDnUwuailo0",
   authDomain: "aaaaaaaa-c3d9e.firebaseapp.com",
@@ -13,5 +7,4 @@ const firebaseConfig = {
   appId: "1:110576771521:web:7ebe418158f3ccf4064f40"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
